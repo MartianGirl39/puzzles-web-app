96f02bb86bd2ed2fb4e13221122361d20a0ef094
Jennifer Forse <forsejennyb@gmail.com>
2024-12-31 23:05:22 -0600

    enough for demo

--- src/screens/futoshiki/Futoshiki.jsx
+++ src/screens/futoshiki/Futoshiki.jsx
@@ -1,11 +1,11 @@
 import { findErrorsForFutoshiki } from "../../features/game/api/checkGame";
-import { getNewFutoshikiBoard, /*mockGetNewFutoshikiBoard*/ } from "../../features/game/api/fetchGame"
+import { mockGetNewFutoshikiBoard } from "../../features/game/api/fetchGame"
 import { Game } from "../../features/game/Game"
 // import { useParams } from "react-router-dom";
 
 export const Futoshiki = () => {
     // const { size } = useParams();
     return (
-        <Game size={9} fetchHandler={getNewFutoshikiBoard} errorCheckHandler={findErrorsForFutoshiki}/>
+        <Game size={9} fetchHandler={mockGetNewFutoshikiBoard} errorCheckHandler={findErrorsForFutoshiki}/>
     )
 }
--- src/screens/sudoku/Sudoku.jsx
+++ src/screens/sudoku/Sudoku.jsx
@@ -1,9 +1,9 @@
 import { Game } from "../../features/game/Game"
 import { findErrorsForSudoku } from "../../features/game/api/checkGame"
-import { /*getNewMockSudokuBoard,*/ getNewSudokuBoard } from "../../features/game/api/fetchGame"
+import { getNewMockSudokuBoard } from "../../features/game/api/fetchGame"
 
 export const Sudoku = () => {
     return (
-        <Game size={9} fetchHandler={getNewSudokuBoard} errorCheckHandler={findErrorsForSudoku}/>
+        <Game size={9} fetchHandler={getNewMockSudokuBoard} errorCheckHandler={findErrorsForSudoku}/>
     )
 }
